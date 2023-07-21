a9dedbd66d31502c4209c0c2af713b14a381b0e3
Yogi Alexius Naibaho <yogialexiusn@gmail.com>
2023-07-21 11:39:20 +0700

    first commit

---
+++ src/main/java/com/merdekacloud/Springjpaexample/Entity/Product.java
@@ -0,0 +1,34 @@
+package com.merdekacloud.Springjpaexample.Entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name ="product")
+//@Table(name = "stock_daily_record", catalog = "mkyongdb",
+//        uniqueConstraints = @UniqueConstraint(columnNames = "DATE"))
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "productId", unique = true, nullable = false)
+    private Long productId;
+
+    @Column(name = "productName", unique = true, nullable = false, length = 100)
+    private String productName;
+
+    @Column(name = "productPrice", nullable = false)
+    private BigDecimal productPrice;
+
+    //technically optional but still needed to save data
+    @Column(name = "category_id", nullable = false)
+    private Long category_id;
+
+}
